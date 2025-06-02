0a80133ca23d4a184a2f8368ca01422c94ae343a
VicenteIligaray <vicente.iligaray@uc.cl>
2025-06-01 22:47:06 -0400

    Cleanup 1D sigmoid

--- 1D/sigmoid/non-parametric/Problem.py
+++ 1D/sigmoid/non-parametric/Problem.py
@@ -1,6 +1,17 @@
 import jax.numpy as jnp
 class Problem:
     def __init__(self):
+        """
+        Initializes the 1D elliptic problem.
+
+        Parameters:
+        - a (float): Left endpoint of the domain.
+        - b (float): Right endpoint of the domain.
+        - s (float): Parameter for the problem, affecting the boundary conditions.
+        - alpha (float): Parameter for the problem, affecting the boundary conditions.
+        - g0 (float): Dirichlet boundary condition at x=a.
+        - g1 (float): Neumann boundary condition at x=b.
+        """
         self.a     = 0.0  # Left endpoint of the domain
         self.b     = 1.0  # Right endpoint of the domain
         self.s     = 0.5
